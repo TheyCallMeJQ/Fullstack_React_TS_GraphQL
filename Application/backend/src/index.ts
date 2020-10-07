7e2a9d24abb100f4f59b2b49af985c2f8822d4f2
Jacques-Philippe Amiot <jaquesmyster@hotmail.com>
2020-10-07 11:16:33 -0400

    Updated notes to contain how-to for format on save for VS, about to define first entity

--- Application/backend/src/index.ts
+++ Application/backend/src/index.ts
@@ -1,3 +1,13 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
 
+const main = async () => {
+  console.log("Hello orld");
+  const orm = await MikroORM.init({
+    dbName: "lireddit",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+};
 
-console.log('Hello orld');
+main();
