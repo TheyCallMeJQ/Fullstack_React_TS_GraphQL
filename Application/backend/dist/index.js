850810b3695ac63ada81d8da4defeafe815ea62f
Jacques-Philippe Amiot <jaquesmyster@hotmail.com>
2020-10-07 12:55:26 -0400

    Fixed problems with code, at bug with insertNative

--- Application/backend/dist/index.js
+++ Application/backend/dist/index.js
@@ -17,10 +17,11 @@ const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
+    yield orm.getMigrator().up();
     const post = orm.em.create(Post_1.Post, { title: "My first post" });
     yield orm.em.persistAndFlush(post);
     console.log("--------------SQL 2-------------------");
-    yield orm.em.persistAndFlush(post);
+    yield orm.em.nativeInsert(Post_1.Post, { title: "My first title 2" });
 });
 main().catch((e) => {
     console.log(e);
--- Application/backend/src/index.ts
+++ Application/backend/src/index.ts
@@ -1,17 +1,18 @@
 import { MikroORM } from "@mikro-orm/core";
+
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-
 import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
   //Create a new Post (does not affect db)
   const post = orm.em.create(Post, { title: "My first post" });
   await orm.em.persistAndFlush(post);
   console.log("--------------SQL 2-------------------");
-  await orm.em.persistAndFlush(post);
+  await orm.em.nativeInsert(Post, { title: "My first title 2" });
 };
 
 main().catch((e) => {
