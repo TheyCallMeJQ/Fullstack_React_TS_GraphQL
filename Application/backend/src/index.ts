9b3544c4065259f9e82cd2a0f6c7c732ad56fd04
Jacques-Philippe Amiot <jaquesmyster@hotmail.com>
2020-10-07 19:53:29 -0400

    Removed dist from things for git to keep track of. Implemented hello Query resolver, about to make some CRUD with MikroORM

--- Application/backend/src/index.ts
+++ Application/backend/src/index.ts
@@ -1,21 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 
-import express from "express";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  const app = express();
   const PORT = 4000;
+  const app = express();
 
-  app.get("/", (req, res) => {
-    res.send("Hello world from Express!");
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   });
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
