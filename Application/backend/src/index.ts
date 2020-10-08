f27068bdf9ca1f083b6b4bf772918a28d7f76818
Jacques-Philippe Amiot <jaquesmyster@hotmail.com>
2020-10-07 20:24:39 -0400

    About to combine apollo-server-express with MikroORM for CRUD ops

--- Application/backend/src/index.ts
+++ Application/backend/src/index.ts
@@ -7,6 +7,7 @@ import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -17,9 +18,10 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
