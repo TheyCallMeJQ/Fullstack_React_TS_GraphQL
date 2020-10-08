d5ed1248795728d621886c564bb669a4c9ea8702
Jacques-Philippe Amiot <jaquesmyster@hotmail.com>
2020-10-08 17:42:04 -0400

    Implemented user register, and updated docs

--- Application/backend/src/resolvers/user.ts
+++ Application/backend/src/resolvers/user.ts
@@ -1,62 +1,27 @@
 import { MyContext } from "src/types";
-// import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
-import { Ctx, Query, Resolver } from "type-graphql";
+import argon2 from "argon2";
+import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
 import { User } from "../entities/User";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field(() => String)
+  password: string;
+}
 
 @Resolver()
 export class UserResolver {
-  @Query(() => [User])
-  users(@Ctx() { em }: MyContext): Promise<User[]> {
-    return em.find(User, {});
+  @Mutation(() => User)
+  async register(
+    @Ctx() { em }: MyContext,
+    @Arg("input", () => UsernamePasswordInput) input: UsernamePasswordInput
+  ) {
+    const { username, password } = input;
+    const hashedPassword = await argon2.hash(password);
+    const user = em.create(User, { username, password: hashedPassword });
+    await em.persistAndFlush(user);
+    return user;
   }
-
-  //   @Query(() => Post, { nullable: true })
-  //   post(
-  //     @Ctx() { em }: MyContext,
-  //     @Arg("id", () => Int) id: number
-  //   ): Promise<Post | null> {
-  //     const post = em.findOne(Post, { id });
-  //     return post;
-  //   }
-
-  //   @Mutation(() => Post)
-  //   async createPost(
-  //     @Ctx() { em }: MyContext,
-  //     @Arg("title", () => String) title: string
-  //   ): Promise<Post> {
-  //     const post = em.create(Post, { title });
-  //     await em.persistAndFlush(post);
-  //     return post;
-  //   }
-
-  //   @Mutation(() => Post, { nullable: true })
-  //   async updatePost(
-  //     @Ctx() { em }: MyContext,
-  //     @Arg("id", () => Number) id: number,
-  //     @Arg("title", () => String, { nullable: true }) title: string
-  //   ): Promise<Post | null> {
-  //     const post = await em.findOne(Post, { id });
-  //     if (!post) {
-  //       return null;
-  //     }
-  //     if (typeof title !== undefined) {
-  //       post.title = title;
-  //       await em.persistAndFlush(post);
-  //     }
-  //     return post;
-  //   }
-
-  //   @Mutation(() => Boolean)
-  //   async deletePost(
-  //     @Ctx() { em }: MyContext,
-  //     @Arg("id", () => Number) id: number
-  //   ): Promise<boolean> {
-  //     try {
-  //       await em.nativeDelete(Post, { id });
-  //       return true;
-  //     } catch (e) {
-  //       console.log("Error", e);
-  //     }
-  //     return false;
-  //   }
 }
