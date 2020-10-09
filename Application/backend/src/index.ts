faaca62d5e6d8bc41820d44f5b5b32a445f04efd
Jacques-Philippe Amiot <jaquesmyster@hotmail.com>
2020-10-09 12:15:05 -0400

    Added redis store to context, about to use it in user resolvers

--- Application/backend/src/index.ts
+++ Application/backend/src/index.ts
@@ -14,9 +14,7 @@ import { UserResolver } from "./resolvers/user";
 import connectRedis from "connect-redis";
 import redis from "redis";
 import session from "express-session";
-
-let RedisStore = connectRedis(session);
-let redisClient = redis.createClient();
+import { MyContext } from "./types";
 
 const main = async () => {
   //Connect to MikroORM
@@ -27,11 +25,23 @@ const main = async () => {
   const PORT = 4000;
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
   app.use(
     session({
       name: "qid",
-      store: new RedisStore({ client: redisClient }),
-      secret: "keyboard cat",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        //No access to cookie from frontend
+        httpOnly: true,
+        //Set cookie lifespan to 10 years
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        //Only work for https
+        secure: __prod__,
+        sameSite: "lax", //csrf
+      },
+      secret: "hbfwelmxlzwjdcdllw",
       resave: false,
     })
   );
@@ -41,7 +51,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
