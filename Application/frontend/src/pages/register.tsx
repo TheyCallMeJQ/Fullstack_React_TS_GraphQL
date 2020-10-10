09704c157f51aab95212e04899291e40e451299f
Jacques-Philippe Amiot <jaquesmyster@hotmail.com>
2020-10-10 16:32:45 -0400

    Added urql client to _app, about to implement login mutation via urql hooks

--- Application/frontend/src/pages/register.tsx
+++ Application/frontend/src/pages/register.tsx
@@ -13,31 +13,6 @@ import { InputField } from "../components/InputField";
 
 interface registerProps {}
 
-// const Register: React.FC<registerProps> = ({}) => {
-//   return (
-//     <Wrapper variant="small">
-//       <Formik
-//         initialValues={{ username: "", password: "" }}
-//         onSubmit={(values) => console.log(values)}
-//       >
-//         {(values, handleChange) => (
-//           <Form>
-//             <FormControl>
-//               <FormLabel htmlFor="username">Username</FormLabel>
-//               <Input
-//                 value={values.username}
-//                 onChange={handleChange}
-//                 id="username"
-//                 placeholder="username"
-//               />
-//               {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-//             </FormControl>
-//           </Form>
-//         )}
-//       </Formik>
-//     </Wrapper>
-//   );
-// };
 const Register: React.FC<registerProps> = ({}) => {
   return (
     <Wrapper variant="small">
